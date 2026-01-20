0ccac6a2b8922f38a891a038879c978fb9d74a92
Szentgyörgyi Péter <peti0202@windowslive.com>
2026-01-20 07:33:43 +0100

    Céljaink szekció kinézete

--- resources/js/Pages/Main.vue
+++ resources/js/Pages/Main.vue
@@ -113,29 +113,31 @@ const toggleMenu = () => {
   </section>
 
   <!--Céljaink szekció-->
-  <section id="goals" class="font-open px-2">
-    <div class="mb-10 flex flex-col sm:flex-row sm:items-center gap-5">
-      <div class="flex flex-col gap-5 mb-10 sm:basis-1/2">
-        <h2 class="text-center font-patrick font-medium text-3xl my-8">Tanulószobánk célja</h2>
-        <p>Egy barátságos környezet kialakítása a gyerekek számára, ahol könnyebb és örömtelibb a tanulás. Nálunk nem csak leckét írnak, hanem valódi segítséget kapnak az iskolai tananyag megértéséhez és elsajátításához.</p>
-        <p>Kis létszámú csoportjaink lehetővé teszik az egyéni figyelmet, így minden gyermek saját tempójában fejlődhet. A házi feladatok elkészítésén és a tananyag átismétlésén túl arra próbálunk törekedni, hogy magabiztosabbá váljanak, önállóan is tudjanak tanulni.</p>
+  <section id="goals" class="font-open px-2 md:py-10 flex justify-center">
+    <div class="max-w-[1200px]">
+      <div class="mb-10 sm:mb-20 flex flex-col sm:flex-row sm:items-center gap-5">
+        <div class="flex flex-col gap-5 mb-10 sm:basis-1/2 sm:mb-0 lg:leading-loose">
+          <h2 class="text-center font-patrick font-medium text-3xl md:text-4xl lg:text-5xl my-8">Tanulószobánk célja</h2>
+          <p>Egy barátságos környezet kialakítása a gyerekek számára, ahol könnyebb és örömtelibb a tanulás. Nálunk nem csak leckét írnak, hanem valódi segítséget kapnak az iskolai tananyag megértéséhez és elsajátításához.</p>
+          <p>Kis létszámú csoportjaink lehetővé teszik az egyéni figyelmet, így minden gyermek saját tempójában fejlődhet. A házi feladatok elkészítésén és a tananyag átismétlésén túl arra próbálunk törekedni, hogy magabiztosabbá váljanak, önállóan is tudjanak tanulni.</p>
+        </div>
+        <div class="flex justify-center sm:basis-1/2 shrink-0">
+          <img class="rounded-md w-64 lg:w-80 shadow-lg" src="/images/irogep.webp" alt="írógép">
+        </div>
       </div>
-      <div class="flex justify-center sm:basis-1/2 shrink-0">
-        <img class="rounded-md w-64 shadow-lg" src="/images/irogep.webp" alt="írógép">
-      </div>
-    </div>
 
-    <div class="mb-10 flex flex-col-reverse sm:flex-row sm:items-center">
-      <div class="flex justify-center sm:basis-1/2 shrink-0">
-        <img class="rounded-md w-64 shadow-lg" src="/images/tanulas_vagott.webp" alt="írógép">
+      <div class="mb-10 flex flex-col-reverse sm:flex-row sm:items-center">
+        <div class="flex justify-center sm:basis-1/2 shrink-0">
+          <img class="rounded-md w-64 lg:w-80 shadow-lg" src="/images/tanulas_vagott.webp" alt="írógép">
+        </div>
+        <div class="flex flex-col gap-5 mb-10 sm:basis-1/2 lg:leading-loose">
+          <p>Nagy hangsúlyt fektetünk a szociális készségek fejlesztésére is. Számos szabadtéri játékkal foglaljuk le őket a tanulás végeztével. Programjaink során a gyerekek nem csak tudással, hanem új barátságokkal is gazdagodnak. A város különböző iskoláiba járnak, itt lehetőség van egymás megismerésére.</p>
+          <p>Ha szeretné, hogy gyermeke olyan közösségben tanuljon, ahol figyelem és gondoskodás veszi körül, akkor nálunk a helye. Mi azért dolgozunk, hogy minden gyereknek sikerélménye legyen és jó jegyek legyenek a bizonyítványban.</p>
+        </div>
       </div>
-      <div class="flex flex-col gap-5 mb-10 sm:basis-1/2">
-        <p>Nagy hangsúlyt fektetünk a szociális készségek fejlesztésére is. Számos szabadtéri játékkal foglaljuk le őket a tanulás végeztével. Programjaink során a gyerekek nem csak tudással, hanem új barátságokkal is gazdagodnak. A város különböző iskoláiba járnak, itt lehetőség van egymás megismerésére.</p>
-        <p>Ha szeretné, hogy gyermeke olyan közösségben tanuljon, ahol figyelem és gondoskodás veszi körül, akkor nálunk a helye. Mi azért dolgozunk, hogy minden gyereknek sikerélménye legyen és jó jegyek legyenek a bizonyítványban.</p>
+      <div>
+        <h2 class="text-4xl text-center font-patrick">Mert együtt tényleg könnyebb a tanulás!</h2>
       </div>
-    </div>
-    <div>
-      <h2 class="text-4xl text-center font-patrick">Mert együtt tényleg könnyebb a tanulás!</h2>
     </div>
   </section>
 
